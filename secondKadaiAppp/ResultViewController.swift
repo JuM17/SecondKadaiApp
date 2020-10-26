18d8042d0a8d364ae295d1b7370380974d63f3cf
Jumpei.Iwauchi <kiyokibarock0107@gmail.com>
2020-10-26 20:13:26 +0900

    Lesson4を追加した。

---
+++ secondKadaiAppp/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  secondKadaiAppp
+//
+//  Created by 岩内順平 on 2020/10/26.
+//  Copyright © 2020 Jumpei Iwauchi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+
+}
