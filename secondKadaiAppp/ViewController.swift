8aec02c99c9119d72cb6ba06ae1579a3f1bd34b6
Jumpei.Iwauchi <kiyokibarock0107@gmail.com>
2020-10-26 17:09:09 +0900

    Initial Commit

---
+++ secondKadaiAppp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondKadaiAppp
+//
+//  Created by 岩内順平 on 2020/10/26.
+//  Copyright © 2020 Jumpei Iwauchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
